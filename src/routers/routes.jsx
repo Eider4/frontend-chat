609fc0b8d5df247bd9ca3456c89f3ec04505b568
jhon1253 <sebastianquinayas@gmail.com>
2024-12-16 10:40:44 -0500

    route

---
+++ src/routers/routes.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Form from '../components/collection/form/Form'
+
+
+const routes = () => {
+  return (
+    <Routes>
+      <Route path="/form" element={<Form />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/chat" element={<Chat />} />
+      <Route path="/notification" element={<Notification />} />
+    </Routes>
+  )
+}
+
+export default routes
